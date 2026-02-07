482477888c28e06f5958e5e58189668b3cf30438
Karam-Najjar-sxw <karam.n2@storexweb.team>
2026-02-07 14:49:23 +0300

    convert-into-mobile-application

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.playstation.k',
+  appName: 'playstation-lounge-dashboard',
+  webDir: 'dist/playstation-lounge-dashboard/browser'
+};
+
+export default config;
